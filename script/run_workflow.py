bc4a7990bc09ec9ad53fe326a98c0ca711022faa
Alison Tintera <amtintera@gmail.com>
2025-12-10 10:42:47 -0500

    Uncomment spacy/gemini classification

--- script/run_workflow.py
+++ script/run_workflow.py
@@ -5,7 +5,7 @@ import time
 
 def main():
     # Train Bag of Words model
-    
+
     subprocess.run(
         [sys.executable, str(Path(__file__).parent.parent/'models'/'bag_of_words'/'bag_of_words_model.py')],
         check=True
@@ -28,11 +28,11 @@ def main():
     end_time = time.time()
     print(f"Total time for processing files and testing: {end_time - start_time:.2f} seconds")
 
-    # # Evaluate SpaCy/Gemini model
-    # subprocess.run(
-    #     [sys.executable, str(Path(__file__).parent.parent/'nlp'/'spacy_feature_extraction.py')],
-    #     check=True
-    # )
+    # Evaluate SpaCy/Gemini model
+    subprocess.run(
+        [sys.executable, str(Path(__file__).parent.parent/'nlp'/'spacy_feature_extraction.py')],
+        check=True
+    )
 
     # Compare results between Bag of Words and SpaCy/Gemini
     subprocess.run(
